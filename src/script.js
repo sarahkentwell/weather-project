daf3513c195b9f20bf04274d44131a28fc07fb5f
Sarah Kentwell <hello@sarahkentwell.com>
2021-03-20 13:40:15 +1100

    Added function to format and display day and time

---
+++ src/script.js
@@ -0,0 +1,32 @@
+function formatDate(date) {
+  let dayIndex = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ]
+
+  let day = days[dayIndex];
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+
+  return `${day} ${hours}:${minutes}`;
+}
+
+
+let dateElement = document.querySelector("#time");
+let currentTime = new Date();
+dateElement.innerHTML = formatDate(currentTime);
