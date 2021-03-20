b62e39b1f9be1d4f86d388f42facf9b9a524e1ba
Sarah Kentwell <hello@sarahkentwell.com>
2021-03-20 14:09:25 +1100

    Created functions to display results

--- src/script.js
+++ src/script.js
@@ -27,6 +27,67 @@ function formatDate(date) {
 }
 
 
-let dateElement = document.querySelector("#time");
+function displayWeather(response) {
+  celsiusTemperature = response.data.main.temp;
+  document.querySelector("#city").innerHTML = response.data.name;
+  document.querySelector("#description").innerHTML = response.data.weather[0].main;
+  document.querySelector("#temp").innerHTML = Math.round(celsiusTemperature);
+  document.querySelector("#humidity").innerHTML = response.data.main.humidity;
+  document.querySelector("#wind-speed").innerHTML = Math.round(response.data.wind.speed);
+  document.querySelector("#weather-icon").setAttribute("src", `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`);
+  document.querySelector("#weather-icon").setAttribute("alt", response.data.weather[0].description);
+}
+
+
+function searchCity(city) {
+  let apiKey = "b4224e6e4fef229c45c5fd63be38ff06";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  
+  axios.get(apiUrl).then(displayWeather);
+}
+
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let city = document.querySelector("#search-field").value;
+  searchCity(city);
+}
+
+
+function displayFahrenheitTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
+  let temperatureElement = document.querySelector("#temp");
+  let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
+  temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
+}
+
+
+function displayCelsiusTemp(event) {
+  event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
+  let temperatureElement = document.querySelector("#temp");
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+}
+
+
+let celsiusTemperature = null;
+
+
+let DateElement = document.querySelector("#time");
 let currentTime = new Date();
-dateElement.innerHTML = formatDate(currentTime);
+DateElement.innerHTML = formatDate(currentTime);
+
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemp); 
+
+let celsiusLink = document.querySelector("#celsius");
+celsiusLink.addEventListener("click", displayCelsiusTemp);
+
+
+searchCity("New York");
